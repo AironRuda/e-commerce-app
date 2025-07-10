8db641c973565ccf1a3f8307989d7eb9996e3e87
AironRuda <airon.rudaa@gmail.com>
2025-07-10 17:57:12 -0500

    fix: solve resumen products justify

--- src/app/components/modals/CartResume.tsx
+++ src/app/components/modals/CartResume.tsx
@@ -63,7 +63,7 @@ const CartResume = () => {
         <p className="text-2xl font-bold">Mi bolsa</p>
       </div>
       <section className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full mt-4">
-        <div className="col-span-1 flex flex-col justify-between gap-4 h-[70vh] md:h-[60vh] scrollbar-hide rounded-2xl p-4 w-full overflow-y-auto">
+        <div className="col-span-1 flex flex-col gap-4 h-[70vh] md:h-[60vh] scrollbar-hide rounded-2xl p-4 w-full overflow-y-auto">
           {cartProducts.map((product) => (
             <CartProductCard
               key={product.id}
